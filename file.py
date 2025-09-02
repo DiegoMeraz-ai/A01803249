59da16024b499ebc3d70eea95865f4a53aa77dbd
DiegoMeraz-ai <a01803249@tec.mx>
2025-09-02 08:56:16 -0600

    Create file.py
    ejercicios 1 2 y 3

---
+++ file.py
@@ -0,0 +1,13 @@
+
+# Ejercicio 1
+donas = int(input("Donas: "))
+print(donas // 12)
+
+# Ejercicio 2
+a = int(input("Número 1: "))
+b = int(input("Número 2: "))
+print(a % b)
+
+# Ejercicio 3
+s = int(input("Segundos: "))
+print(s // 3600, (s % 3600) // 60, s % 60)
